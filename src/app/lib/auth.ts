d68367b5354fc195f0cece169af3187e15b8bea7
Fajla Rabby <fajlarabbyla24@gmail.com>
2026-05-07 14:24:26 +0600

    feat: implement authentication system using Better Auth and configure database schemas for users, generations, and payments

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,10 +1,54 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { UserRole, UserStatus } from "../../generated/prisma/enums";
 import { prisma } from "./prisma";
 // If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false,
+  },
+  emailVerification: {
+    sendOnSignIn: false,
+    sendOnSignUp: false,
+    autoSignInAfterVerification: true,
+  },
+  user: {
+    additionalFields: {
+      emailVerified: {
+        type: "boolean",
+        required: true,
+        defaultValue: true,
+      },
+      phone: {
+        type: "string",
+        required: false,
+        defaultValue: null,
+      },
+      role: {
+        type: "string",
+        required: true,
+        defaultValue: UserRole.USER,
+      },
+      status: {
+        type: "string",
+        required: true,
+        defaultValue: UserStatus.ACTIVE,
+      },
+      isDeleted: {
+        type: "boolean",
+        required: true,
+        defaultValue: false,
+      },
+      deletedAt: {
+        type: "date",
+        required: false,
+        defaultValue: null,
+      },
+    }
+  }
 });
